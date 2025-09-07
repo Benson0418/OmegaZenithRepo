eeff5caeb3275772c4cc0ed9d207d01c78869c5c
Benson0418 <dges030712@gmail.com>
2025-09-07 15:41:51 +0800

    "dsfsdfasdfsdfasfasdfasdfasdfsdfsadfasdfasdfasfasdfasdfasdfsadfasdfasdfasdf commit."

--- src/UltraPkg/SuperClass.java
+++ src/UltraPkg/SuperClass.java
@@ -3,5 +3,6 @@ package UltraPkg;
 public class SuperClass{
     public static void main(String[] args) {
         System.out.println("Hello Supreme, Super Class here!");
+        System.out.println("Already Done, Trust!");
     }
 }
